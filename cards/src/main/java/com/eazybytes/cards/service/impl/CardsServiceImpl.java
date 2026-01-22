1614e21e09474a8db8fda3d25bc3600873a83bae
disha <dishanayak395@gmail.com>
2026-01-22 19:34:41 +0530

    Global Exception Handler added

---
+++ cards/src/main/java/com/eazybytes/cards/service/impl/CardsServiceImpl.java
@@ -0,0 +1,72 @@
+package com.eazybytes.cards.service.impl;
+
+import com.eazybytes.cards.constants.CardsConstants;
+import com.eazybytes.cards.dto.CardsDto;
+import com.eazybytes.cards.entity.Cards;
+import com.eazybytes.cards.exception.CardAlreadyExistsException;
+import com.eazybytes.cards.repository.CardsRepository;
+import com.eazybytes.cards.service.ICardsService;
+
+import java.util.Optional;
+import java.util.Random;
+
+public class CardsServiceImpl implements ICardsService {
+
+    private CardsRepository cardsRepository;
+
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     */
+    @Override
+    public void createCard(String mobileNumber) {
+        Optional<Cards> optionalCards = cardsRepository.findByMobileNumber(mobileNumber);
+        if(!optionalCards.isEmpty()){
+            throw new CardAlreadyExistsException("Card already registered with given Mobile Number");
+        }
+        cardsRepository.save(createNewCard(mobileNumber));
+    }
+
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     * @return the new card details
+     */
+    private Cards createNewCard(String mobileNumber){
+        Cards newCard = new Cards();
+        long randomCardNumber = 100000000000L + new Random().nextInt(900000000);
+        newCard.setCardNumber(Long.toString(randomCardNumber));
+        newCard.setMobileNumber(mobileNumber);
+        newCard.setCardType(CardsConstants.CREDIT_CARD);
+        newCard.setTotalLimit(CardsConstants.NEW_CARD_LIMIT);
+        newCard.setAmountUsed(0);
+        newCard.setAvailableAmount(CardsConstants.NEW_CARD_LIMIT);
+        return newCard;
+
+    }
+
+    /**
+     * @param mobileNumber - Input mobile Number
+     * @return Card Details based on a given mobileNumber
+     */
+    @Override
+    public CardsDto fetchCard(String mobileNumber) {
+        return null;
+    }
+
+    /**
+     * @param cardsDto - CardsDto Object
+     * @return boolean indicating if the update of card details is successful or not
+     */
+    @Override
+    public boolean updateCard(CardsDto cardsDto) {
+        return false;
+    }
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of card details is successful or not
+     */
+    @Override
+    public boolean deleteCard(String mobileNumber) {
+        return false;
+    }
+}
