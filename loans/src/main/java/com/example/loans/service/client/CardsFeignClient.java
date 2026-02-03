099e73f62bf5404492121ba56221f17b2389f488
disha <dishanayak395@gmail.com>
2026-02-03 19:06:05 +0530

    add Feign Client related changes for invoking other microservices

---
+++ loans/src/main/java/com/example/loans/service/client/CardsFeignClient.java
@@ -0,0 +1,14 @@
+package com.example.loans.service.client;
+
+import com.example.loans.dto.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("loans")
+public interface CardsFeignClient {
+    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    public ResponseEntity<LoansDto> fetchLoansDetails(@RequestParam String mobileNumber);
+
+}
